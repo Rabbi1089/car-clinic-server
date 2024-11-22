d88bbf8e875663397cadbeca2ed307d317fa62d4
Rabbi1089 <rabbi.ext@gmail.com>
2024-11-23 01:41:47 +0600

    add filter and search

--- index.js
+++ index.js
@@ -9,25 +9,22 @@ console.log("Mongo started successfully");
 const app = express();
 const port = process.env.port || 5000;
 
-
 const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:5174'
-  ],
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
-}
+};
 
-app.use(cors(
-  {
-    origin : ['http://localhost:5173'],
-    credentials : true
-  }));  
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.t241ufd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,30 +38,28 @@ const client = new MongoClient(uri, {
 
 //middleware
 
-const logger = async (req, res,  next) => {
-  console.log('logger' , req.originalUrl)
-  next()
-}
+const logger = async (req, res, next) => {
+  //console.log('logger' , req.originalUrl)
+  next();
+};
 
 //create middleware primary step
-const verifyToken = async (req, res , next) => {
-  const token = req.cookies.token
-  console.log('verifyToken', token);
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies.token;
+  console.log("verifyToken", token);
   if (!token) {
-    return res.status(401).send({message : 'Unauthorized token'})
+    return res.status(401).send({ message: "Unauthorized token" });
   }
   //// verify a token symmetric from jwt.io
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
-      return res.status(401).send({message : 'Unauthorized access token'})
+      return res.status(401).send({ message: "Unauthorized access token" });
     }
-      console.log('decoded value of token' , decoded)
-      req.user = decoded
-  })
-  next()
-}
-
-
+    console.log("decoded value of token", decoded);
+    req.user = decoded;
+  });
+  next();
+};
 
 async function run() {
   try {
@@ -73,11 +68,25 @@ async function run() {
     const serviceCollection = client.db("carClinic").collection("services");
     const bookingCollection = client.db("carClinic").collection("bookings");
     app.get("/services", logger, async (req, res) => {
-      const cursor = serviceCollection.find();
+      const filter = req.query.sort;
+      const sText = req.query.text;
+     
+      console.log(filter);
+      const query = {
+        title : {
+          $regex : sText, $options: 'i'
+        }
+      };
+      const options = {
+        sort: { price: filter === "asc" ? 1 : -1 },
+      };
+      //filter receive  value form query , in short price will get (1 or -1 with filter conditional value)
+      const cursor = serviceCollection.find(query, options);
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    //verify token , withcredential add on client site
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -87,17 +96,20 @@ async function run() {
       };
 
       const result = await serviceCollection.findOne(query, options);
-      console.log(result);
+
       res.send(result);
     });
 
     //booking specific data
+    //verify token , need {withCredentials : true}) add on client site
     app.get("/booking", logger, verifyToken, async (req, res) => {
-      let email = req.query.email
-      console.log(email)
-      console.log('user information from verify token' , req.user.email)
+      let email = req.query.email;
+      // This email comes from url
+      //this comes from
+
       if (email !== req.user.email) {
-        return res.status(401).send({message : 'Unauthorized token email'})
+        //req.user.email comes from verify token middleware
+        return res.status(401).send({ message: "Unauthorized token email" });
       }
       //console.log('Coming  from token' , req.cookies.token)
       let query = {};
@@ -108,20 +120,19 @@ async function run() {
       res.send(result);
     });
 
-
     //delete a specific booking
     app.delete("/booking/:id", async (req, res) => {
       let id = req.params.id;
-      console.log(id);
+
       const query = { _id: new ObjectId(id) };
-      console.log(query);
+
       const result = await bookingCollection.deleteOne(query);
       res.send(result);
     });
 
     app.patch("/booking/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+
       const filter = { _id: new ObjectId(id) };
       const updateBooking = req.body;
       const updateDoc = {
@@ -129,7 +140,7 @@ async function run() {
           status: updateBooking.status,
         },
       };
-      console.log(updateBooking);
+
       const result = await bookingCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
@@ -142,13 +153,17 @@ async function run() {
 
     //--------------------------------auth related api
     //step 1 - create token
-    app.post("/jwt", logger,  async (req, res) => {
+    app.post("/jwt", logger, async (req, res) => {
       const user = req.body;
       //user is coming from client
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
       //generate a token
-      console.log("come from jwt", token);
-      res.cookie('token', token, {httpOnly: true, secure: false}).send({ success: true })
+
+      res
+        .cookie("token", token, { httpOnly: true, secure: false })
+        .send({ success: true });
       //set token in cookie , to set a cookie need to ensure cors option and {withCredentials: true} in client site
     });
 
